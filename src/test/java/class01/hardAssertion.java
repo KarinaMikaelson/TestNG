58688b60d7356465478a93af29da47c927d3ab2b
KarinaMikaelson <mikael.kar7@gmail.com>
2024-05-22 15:31:15 -0700

    4 commit

--- src/test/java/class01/hardAssertion.java
+++ src/test/java/class01/hardAssertion.java
@@ -20,30 +20,35 @@ public class hardAssertion {
         driver.get("http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
-    @Test(dependsOnMethods = {"loginBtnDisplayedAndEnabled"})
+    @Test(dependsOnMethods = {"loginBtnIsEnabled"})
     public void loginVerification(){
         WebElement username = driver.findElement (By.xpath ("//input[@name='txtUsername']"));
         username. sendKeys("Admin");
         WebElement password = driver.findElement (By.xpath ("//input[@id = 'txtPassword']"));
         password. sendKeys("HAS TO FAIL");
-        WebElement loginBtn = driver.findElement (By.xpath("//input[@name='Submit']")) ;
+        WebElement loginBtn = driver.findElement (By.id("btnLogin")) ;
         loginBtn.click();
 
 //          verify that correct INVALID CREDENTIALS displayed
         String actualErrorMsg = driver.findElement(By.id("spanMessage")).getText();    //inspect "Invalid password" element \
         Assert.assertEquals(actualErrorMsg,"Invalid credentials");
+        System.out.println("Correct invalid credentials are displayed");
 
     }
     @Test
-    public void loginBtnDisplayedAndEnabled(){
-        WebElement loginBtn = driver.findElement (By.xpath("//input[@name='Submit']")) ;
+    public void loginBtnDisplayed(){
+        WebElement loginBtn = driver.findElement (By.id("btnLogin")) ;
         Assert.assertTrue(loginBtn.isDisplayed(),"login button is not displayed");
+        System.out.println("Login button is displayed");
+    }
+    @Test(dependsOnMethods = {"loginBtnDisplayed"})
+    public void loginBtnIsEnabled(){
+        WebElement loginBtn = driver.findElement (By.id("btnLogin")) ;
         Assert.assertTrue(loginBtn.isEnabled(),"login button is disabled");
-        System.out.println("Login button is displayed and enabled");
+        System.out.println("Login button is enabled");
     }
     @AfterMethod
     public void endTest(){
         driver.quit();
-        System.out.println("Test has been executed");
     }
 }
